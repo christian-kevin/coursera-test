5b86259294126ee472ee651a7786d50387562f5c
lenovoKevin-pc <christian.kevinn11@gmail.com>
2016-12-05 15:35:07 +0700

    Lecture 14

---
+++ digestcycle/app.js
@@ -0,0 +1,30 @@
+(function(){
+'use strict';
+
+angular.module('CounterApp',[])
+.controller('CounterController',CounterController);
+
+//Protect function input from minification
+CounterController.$inject = ['$scope'];
+
+function CounterController($scope){
+  $scope.onceCounter = 0;
+  $scope.counter = 0;
+  $scope.showNumberOfWatchers = function(){
+    console.log("# of Watchers", $scope.$$watchersCount);
+  };
+
+  $scope.countOnce = function(){
+    $scope.onceCounter = 1;
+  };
+  $scope.upCounter = function(){
+    $scope.counter++;
+  }
+  $scope.$watch(function(){
+    console.log("Digest Loop Fired!");
+  })
+}
+
+
+
+})();
